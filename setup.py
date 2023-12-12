6d965d39d3cd531b19279ee672a0937e51656c48
lee <786525133@qq.com>
2023-12-12 15:08:51 +0800

    Add a setup script to install the library locally.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="HD44780MCP",
+    version="0.0.1",
+    author="owainm713 & Albert-lee",
+    author_email="",
+    description="In order to adapt to the Raspberry Pi used mcp23008 connected to the 1602lcd program, because the Adafruit_CharLCD class abandoned the Adafruit_CharLCDBackpack method!",
+    url="https://github.com/kevin786525/HD44780-MCP-Python-Module.git",
+    packages=find_packages(),
+    install_requires=["smbus, time"],
+)
